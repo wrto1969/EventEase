4b34b1b4e08a3adb5fec255e3854f166a05afe24
Ed Beltran <wrto69@outlook.com>
2025-03-21 15:20:06 -0400

    registration - added success when registration is set

--- EventEase/obj/Debug/net9.0/EventEase.AssemblyInfo.cs
+++ EventEase/obj/Debug/net9.0/EventEase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64017a770c790b407317a29f6ff7aee0d6a2d1d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10e9ecc645679a16e5c8e38681c570ebc9a5bb5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
